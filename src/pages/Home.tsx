deab92d0b1252e953ca6a1a4af5fd31fa69b7761
semoscow <s.e.moscow@mail.ru>
2026-04-24 12:39:23 +0000

    fix: shift video lower on desktop

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -53,7 +53,7 @@ export default function Home() {
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth >= 768) {
-        setVideoPosition('55% 60%')
+        setVideoPosition('55% 70%')
       } else {
         setVideoPosition('55% center')
       }
