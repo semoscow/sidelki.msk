0ace2b8a43eb5f5595b0e9d19da90dde955d28a9
semoscow <s.e.moscow@mail.ru>
2026-04-24 13:12:57 +0000

    fix: position video to show heads at top

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -53,7 +53,7 @@ export default function Home() {
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth >= 768) {
-        setVideoPosition('70% 100%')
+        setVideoPosition('70% 0%')
       } else {
         setVideoPosition('55% center')
       }
