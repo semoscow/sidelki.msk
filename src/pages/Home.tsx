2d0560f9b84c8ced246f91f380358f619a024dcc
semoscow <s.e.moscow@mail.ru>
2026-04-24 13:19:58 +0000

    fix: move video all the way to the right

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -53,7 +53,7 @@ export default function Home() {
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth >= 768) {
-        setVideoPosition('85% 0%')
+        setVideoPosition('100% 0%')
       } else {
         setVideoPosition('55% center')
       }
