f25a3877a052a0f92900b78941ba4b808708ac64
semoscow <s.e.moscow@mail.ru>
2026-04-24 13:02:37 +0000

    fix: move video down in correct direction

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -53,7 +53,7 @@ export default function Home() {
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth >= 768) {
-        setVideoPosition('70% 50%')
+        setVideoPosition('70% 85%')
       } else {
         setVideoPosition('55% center')
       }
